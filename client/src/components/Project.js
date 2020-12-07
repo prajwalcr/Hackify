e337ddde7f4ebee19d9c712c7c2d1c7614f0c8e3
Prajwal Gupta C R <57584330+prajwalcr@users.noreply.github.com>
2020-12-07 18:41:09 +0530

    Update Project.js

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -17,7 +17,7 @@ class Project extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			success: true,
+			err_id: null,
 		};
 	}
 
@@ -25,34 +25,32 @@ class Project extends Component {
 		const { error } = this.props;
 		if (error !== prevProps.error) {
 			if (error.id !== null) {
-				this.setState({ success: error.msg.success });
+				this.setState({ err_id: error.id });
 			} else {
-				this.setState({ success: null });
+				this.setState({ err_id: null });
 			}
 		}
 	}
 	render() {
-		function standby() {
-			document.getElementById('foo').src = './favicon.ico'
-		}
-
 		return (
-			<div className="general">
+			<div className='general'>
 				{this.props.projects[0] ? (
 					<div>
-						<h1 className='project-title' >{this.props.projects[0].title}</h1>
+						<h1 className='project-title'>{this.props.projects[0].title}</h1>
 						<center>
-						<img
-							id="foo"		
-							src={this.props.projects[0].coverPic}
-							alt='Failed to load cover pic'
-							onError="standby()"
-							style={{align:"middle",maxHeight:350,maxWidth:350}}
-						></img>
+							<img
+								id='foo'
+								src={
+									this.props.projects[0].coverPic ||
+									"http://localhost:5000/uploads\\default.jfif"
+								}
+								alt='Failed to load cover pic'
+								onError='standby()'
+								style={{ align: "middle", maxHeight: 350, maxWidth: 350 }}
+							></img>
 						</center>
 						<div
 							className='project-text'
-							
 							dangerouslySetInnerHTML={{
 								__html: this.props.projects[0].content,
 							}}
@@ -60,7 +58,7 @@ class Project extends Component {
 					</div>
 				) : (
 					<div>
-						{!this.state.success ? (
+						{this.state.err_id === "PROJECT_LOAD_FAIL" ? (
 							<div>
 								<Redirect to='/'></Redirect>
 							</div>
