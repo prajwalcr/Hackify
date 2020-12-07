9a395c32188f034f587084649730f8882b3fef07
PurujithM <73056704+PurujithM@users.noreply.github.com>
2020-12-07 17:41:12 +0530

    Add files via upload

---
+++ CardList.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import CardPrint from "./Cards";
+
+class CardList extends Component {
+	componentDidMount() {
+		this.props.getProjects();
+	}
+	render() {
+		var projects = this.props.projects;
+		if (projects) {
+			projects = projects.filter((project) => {
+				return project.isPublic === true;
+			});
+		}
+		console.log(this.props.projects);
+		return (
+			<div className='grid'>
+				{projects.map(function (project, index) {
+					return (
+						<CardPrint
+							key={index}
+							image={project.coverPic}
+							title={project.title}
+							view={`/project/${project._id}`}
+						></CardPrint>
+					);
+				})}
+			</div>
+		);
+	}
+}
+const mapStateToProps = (state) => ({
+	projects: state.project.projects,
+});
+
+export default connect(mapStateToProps, { getProjects })(CardList);
